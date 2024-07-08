23d1aeab3b0b1f5fd0aecf0ee645bb619cd219ca
UsualSpec <98665326+UsualSpec@users.noreply.github.com>
2024-07-08 17:06:25 +0200

    Redirect sampling back to stdout
    Moving data printing back to stdout is needed for autopower devices.

--- src/Sampler.cpp
+++ src/Sampler.cpp
@@ -141,7 +141,7 @@ void Sampler::continuous_print_tick()
 	if (settings::countinous_timestamp_flag) {
 		// I wanted to use duration<float, ratio<1>>, but this resulted in weird constant epoch
 		auto ms_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(timestamp.time_since_epoch()).count();
-		settings::output_stream << ms_since_epoch * 0.001 << ",";
+		std::cout << ms_since_epoch * 0.001 << ",";
 	}
-	settings::output_stream << buf << std::endl;
+	std::cout << buf << std::endl;
 }
